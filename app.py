c94e3b56a7fdd86c349d2c86d01b8a73b26b61f5
Marcel Beyer <marcel.beyer@it-maker.eu>
2019-02-02 23:37:40 +0100

    ui preparation

--- app.py
+++ app.py
@@ -47,9 +47,13 @@ def get_app():
 if __name__ == '__main__':
     app.run()
 
+@app.context_processor
+def sensors():
+    return dict(sensors=['a'])
+
 @app.route('/')
 def home():
-    return 'Hello World!'
+    return render_template('index.html')
 
 @app.route('/install', methods=['GET', 'POST'])
 def install():
