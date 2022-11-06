77245ce8de268d5bd37aabc98a67d596075d3848
Marcel Beyer <marcel.beyer@it-maker.eu>
2022-11-06 01:38:28 +0100

    dockerize

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM debian:stable-slim
+
+MAINTAINER Marcel Beyer "marcel.beyer@it-maker.eu"
+
+RUN apt-get update -y && \
+    apt-get install -y python3-pip python3-dev
+
+COPY ./requirements.txt /app/requirements.txt
+
+WORKDIR /app
+
+RUN pip install -r requirements.txt
+RUN pip install waitress==2.1.2
+
+COPY . /app
+
+RUN chmod u+x ./entrypoint.sh
+ENTRYPOINT [ "./entrypoint.sh" ]
+
---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#
+# Entrypoint for docker image to run tempSrvr.
+#
+# Environment variables to change the default behaviour:
+# Variable		Default
+# TEMPSRV_PORT		5000		The port tempSrvr will provide in the container
+# TEMPSRV_URL		''		The URL prefix flask should use for URL generation
+#
+
+python manage.py db upgrade
+
+if [[ -z "${TEMPSRV_PORT}" ]]; then
+  TEMPSRV_PORT="5000"
+fi
+
+if [[ -z "${TEMPSRV_URL}" ]]; then
+  TEMPSRV_URL=""
+fi
+
+echo "Starting app on port $TEMPSRV_PORT with URL prefix $TEMPSRV_URL ..."
+waitress-serve --port=$TEMPSRV_PORT --url-prefix="$TEMPSRV_URL" --call 'app:get_app'
